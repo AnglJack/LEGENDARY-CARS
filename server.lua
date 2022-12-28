d3b9b0c2a228c451df013aa280d6ad3bab16d645
AnglJack <100953855+AnglJack@users.noreply.github.com>
2022-12-28 17:11:09 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -16,23 +16,23 @@ Citizen.CreateThread(function()
             if curVersion ~= responseText and tonumber(curVersion) < tonumber(responseText) then
                 print("    _/    _/ _/_/_/  ║                   Hey there! Yeah you, thanks for using my car pack!")
                 print("   _/  _/   _/    _/ ║")
-                print("  _/_/     _/    _/  ║ Got stuck? or have a question? or wanna chat with other users? join discord.khandesign.net.")
+                print("  _/_/     _/    _/  ║")
                 print(" _/  _/   _/    _/   ║")
-                print("_/    _/ _/_/_/      ║                       Uh Oh! Looks like PLOKS_CARS is outdated")
+                print("_/    _/ _/_/_/      ║                       Uh Oh! Looks like LEGENDARY_CARS is outdated")
                 print("                     ║ Should be: "..responseText..", currently is: "..curVersion..". Please update it from https://github.com"..updatePath.."")
             elseif tonumber(curVersion) > tonumber(responseText) then
                 print("    _/    _/ _/_/_/  ║                   Hey there! Yeah you, thanks for using my car pack!")
                 print("   _/  _/   _/    _/ ║")
-                print("  _/_/     _/    _/  ║ Got stuck? or have a question? or wanna chat with other users? join discord.khandesign.net.")
+                print("  _/_/     _/    _/  ║")
                 print(" _/  _/   _/    _/   ║")
-                print("_/    _/ _/_/_/      ║                Uh Oh! You somehow skipped a few versions of PLOKS_CARS")
+                print("_/    _/ _/_/_/      ║                Uh Oh! You somehow skipped a few versions of LEGENDARY_CARS")
                 print("                     ║  or the git went offline, if it's still online I advise you to update (or downgrade?)")
             else
                 print("    _/    _/ _/_/_/  ║                   Hey there! Yeah you, thanks for using my car pack!")
                 print("   _/  _/   _/    _/ ║")
-                print("  _/_/     _/    _/  ║ Got stuck? or have a question? or wanna chat with other users? join discord.khandesign.net.")
+                print("  _/_/     _/    _/  ║")
                 print(" _/  _/   _/    _/   ║")
-                print("_/    _/ _/_/_/      ║                   It looks like PLOKS_CARS is up to date, have fun!")
+                print("_/    _/ _/_/_/      ║                   It looks like LEGENDARY_CARS is up to date, have fun!")
             end
         end
     end
